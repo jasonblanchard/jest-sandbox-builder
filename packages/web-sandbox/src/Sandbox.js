9e7331efa8708b5f6930022bb8853e85c24b53d2
Jason Blanchard <jas.blanchard@gmail.com>
2018-04-10 11:45:15 -0400

    Kind of working

--- packages/web-sandbox/src/Sandbox.js
+++ packages/web-sandbox/src/Sandbox.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export default class Sandbox extends Component {
   static defaultProps = {
     registry: {}
+  }
+  
+  constructor(props) {
+    super(props);
+    
+    this._stack = [];
+    this._basePath = window.location.pathname;
   }
   
   render() {
@@ -18,35 +26,51 @@ export default class Sandbox extends Component {
     const registry = this.props.registry;
     console.log(registry);
 
-    // TODO: Render with paths instead of `<ul>`s
     return (
-      <ul>
-        {this._recursivelyRenderRegistryKey(undefined, registry)}
-      </ul>
+      <Router>
+        <div>
+          <ul>
+            {this._recursivelyRenderRegistryKey(undefined, registry)}
+          </ul>
+        </div>
+      </Router>
     );
   }
 
   _recursivelyRenderRegistryKey(topLevelKey, registry) {
     return Object.keys(registry).map(key => {
       if (!registry[key]) return null;
-
+      
+      this._stack.push(key);
+    
       if (registry[key].isLeaf) {
         return (
           <li key={key}>
-            <h2>{key}</h2>
-            {registry[key].component}
+            <Link to={`${this._basePath}/${this._stack.join('/')}`}>{key}</Link>
+            {this._renderComponentPage(registry[key].component)}
           </li>
         );
       }
 
-      return (
+      const result = (
         <li key={key}>
-          <h2>{key}</h2>
+          {key}
           <ul>
             {this._recursivelyRenderRegistryKey(key, registry[key])}
           </ul>
         </li>
       );
+      
+      this._stack.pop();
+      
+      return result;
     });
+  }
+  
+  _renderComponentPage(component) {
+    console.log(this._stack);
+    return (
+      <Route exact path={`${this._basePath}/${this._stack.join('/')}`} render={() => component} />
+    );
   }
 }
