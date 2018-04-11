0300ac2f01614dd167358f57e93d70e605f60007
Jason Blanchard <jas.blanchard@gmail.com>
2018-04-11 16:54:57 -0400

    Cleanup

--- packages/jestbox-webpack-plugin/src/index.js
+++ packages/jestbox-webpack-plugin/src/index.js
@@ -1,21 +1,20 @@
-const webpack = require('webpack');
+import webpack from 'webpack';
 
-function SandboxWebpackPlugin(options) {
-  // Setup the plugin instance with options.....
+class SandboxWebpackPlugin {
+  constructor(options) {
+    // Setup the plugin instance with options...
+  }
+
+  apply(compiler) {
+    compiler.plugin('done', function(compiler) {
+      console.log('Replaced Jest globals with jestbox functions');
+    });
+
+    compiler.options.plugins.push(new webpack.ProvidePlugin({
+      describe: ['jestbox-builder', 'describe'],
+      it: ['jestbox-builder', 'it'],
+    }));
+  }
 }
 
-SandboxWebpackPlugin.prototype.apply = function(compiler) {
-  compiler.plugin('done', function(compiler) {
-    console.log('Replaced Jest globals with sandbox functions');
-  });
-
-  compiler.options.plugins.push(new webpack.ProvidePlugin({
-    describe: ['jestbox-builder', 'describe'],
-    it: ['jestbox-builder', 'it'],
-    expect: ['jestbox-builder', 'expect'],
-    jest: ['jestbox-builder', 'jest'],
-    beforeEach: ['jestbox-builder', 'beforeEach']
-  }));
-};
-
-module.exports = SandboxWebpackPlugin;
+export default SandboxWebpackPlugin;
